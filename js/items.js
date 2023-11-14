359c1420d1a1813f79c6071510dc2e45d1c19645
Lamoreaux, Jachin <jlamoreaux@bethel.jw.org>
2023-11-14 12:58:29 -0500

    item template

---
+++ js/items.js
@@ -0,0 +1,10 @@
+const commonItems = [
+  {
+    name: "brokenWatch",
+    boost: 1,
+    boostType: "add",
+    attr: "critChance",
+    desc: "Its right twice a day",
+    detailedDesc: "Adds a 1% critical chance per stack",
+  },
+];
