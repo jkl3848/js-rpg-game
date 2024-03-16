f9f18b125e50f63e8fd6e21909c9a2c98e7829be
jlamoreaux <jachin@lamfam.net>
2024-03-16 19:32:00 -0400

    vue upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
 import ReactivityTransform from "@vue-macros/reactivity-transform/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue(), ReactivityTransform()],
+  plugins: [Vue(), ReactivityTransform()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
