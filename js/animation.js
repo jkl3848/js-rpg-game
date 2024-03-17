3778f6028a23c641666612b4a3e88ae4960db6c8
jlamoreaux <jachin@lamfam.net>
2024-03-16 20:30:20 -0400

    adjust folder struct

--- js/animation.js
+++ js/animation.js
@@ -7,23 +7,20 @@ export const animation = () => {
     constructor() {
       //Downloads images for use
       this.toLoad = {
-        overworldMap1: "./public/assets/maps/test_map.jpg",
-        combatMap1: "./public/assets/maps/test_combat.png",
-        knightWalking: "./public/assets/characters/hero-walking/knight.png",
-        blurbi: "./public/assets/characters/npcs/blurbi_sprite.png",
-        enemySlime: "./public/assets/characters/enemies/slime_sprite.png",
-        enemyBabyTurtle:
-          "./public/assets/characters/enemies/baby_turtle_sprite.png",
-        enemyBigTurtle:
-          "./public/assets/characters/enemies/big_turtle_sprite.png",
-        enemyTurtle: "./public/assets/characters/enemies/turtle_sprite.png",
-        enemyRat: "./public/assets/characters/enemies/rat_sprite.png",
-        enemyEvilMoose:
-          "./public/assets/characters/enemies/evil_moose_sprite.png",
-        enemyBat: "./public/assets/characters/enemies/bat_sprite.png",
-        iconArrowDown: "./public/assets/icons/arrow_down_icon.png",
-        iconEffectPoison: "./public/assets/icons/poison_icon.png",
-        iconEffectFire: "./public/assets/icons/fire_icon.png",
+        overworldMap1: "assets/maps/test_map.jpg",
+        combatMap1: "assets/maps/test_combat.png",
+        knightWalking: "assets/characters/hero-walking/knight.png",
+        blurbi: "assets/characters/npcs/blurbi_sprite.png",
+        enemySlime: "assets/characters/enemies/slime_sprite.png",
+        enemyBabyTurtle: "assets/characters/enemies/baby_turtle_sprite.png",
+        enemyBigTurtle: "assets/characters/enemies/big_turtle_sprite.png",
+        enemyTurtle: "assets/characters/enemies/turtle_sprite.png",
+        enemyRat: "assets/characters/enemies/rat_sprite.png",
+        enemyEvilMoose: "assets/characters/enemies/evil_moose_sprite.png",
+        enemyBat: "assets/characters/enemies/bat_sprite.png",
+        iconArrowDown: "assets/icons/arrow_down_icon.png",
+        iconEffectPoison: "assets/icons/poison_icon.png",
+        iconEffectFire: "assets/icons/fire_icon.png",
       };
 
       //Holds all images
